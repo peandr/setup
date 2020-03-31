67bb86f5e1f55afe12684077e9befe28254774ef
peandr <peter.andree@hispeed.ch>
2020-03-31 07:35:15 +0000

    added neofetch config

--- al-post-config.sh
+++ al-post-config.sh
@@ -127,6 +127,7 @@ if [ -d /home/$UN/ ]; then
 	sudo ln -sf /home/peandr/.config/neofetch/config.conf /root/.config/neofetch/config.conf
 
 	wget http://raspberrypi/sh/mount-share-linux.sh
+	chmod 700 mount-share-linux.sh
 	mkdir -p /home/$UN/.local/sh
 	mv mount-share-linux.sh /home/$UN/.local/sh/mount-share.sh
 
